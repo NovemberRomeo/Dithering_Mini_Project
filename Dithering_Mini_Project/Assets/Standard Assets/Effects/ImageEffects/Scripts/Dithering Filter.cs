b4084e9d533a865e25bc0e952b213331258d37e1
Nolan Sejr Rux <nrux17@student.aau.dk>
2019-11-04 12:31:21 +0100

    Added script, shaders, and textures

---
+++ Dithering_Mini_Project/Assets/Standard Assets/Effects/ImageEffects/Scripts/Dithering Filter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[ExecuteInEditMode, ImageEffectAllowedInSceneView, RequireComponent(typeof(Camera))]
+
+public class DitheringFilter : MonoBehaviour
+{
+	public Material ditherMaterial;
+	[Range(0.0f, 1.0f)] 
+	public float ditherStrength = 0.1f;
+	[Range(1, 32)]
+	public int colourDepth = 4;
+
+	private void OnRenderImage(RenderTexture src, RenderTexture dest)
+	{
+		ditherMaterial.SetFloat("_DitherStrength", ditherStrength);
+		ditherMaterial.SetInt("_ColourDepth", colourDepth);
+		Graphics.Blit(src, dest, ditherMaterial);
+	}
+	
+	
+}
